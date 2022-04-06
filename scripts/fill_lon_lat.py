a4b274c73e58f9872db576f6c843e0aa2405da9d
jordan-boswell <83526247+jordan-boswell@users.noreply.github.com>
2022-04-05 22:50:30 -0700

    Fixed issue where the last few rows of final.csv had mostly NA values

--- scripts/fill_lon_lat.py
+++ scripts/fill_lon_lat.py
@@ -31,15 +31,5 @@ for i in sc.index:
                 sc.loc[i, 'LONGITUDE'] = response_json['geonames'][0]['lng']
         except Exception:
             pass
- 
-sc.loc[6395, ['LATITUDE', 'LONGITUDE']] = [44.0580, -75.765752]
-sc.loc[6473, ['LATITUDE', 'LONGITUDE']] = [44.545307, -72.000069]
-sc.loc[6491, ['LATITUDE', 'LONGITUDE']] = [18.444247, -66.646407]
-sc.loc[6492, ['LATITUDE', 'LONGITUDE']] = [18.3786, -65.8393]
-sc.loc[6493, ['LATITUDE', 'LONGITUDE']] = [18.0111, -66.6141]
-sc.loc[6494, ['LATITUDE', 'LONGITUDE']] = [18.2388, -66.0352]
-sc.loc[6495, ['LATITUDE', 'LONGITUDE']] = [18.2013, -67.1452]
-sc.loc[6578, ['LATITUDE', 'LONGITUDE']] = [18.2116, -65.7349]
-sc.loc[6579, ['LATITUDE', 'LONGITUDE']] = [18.4704, -67.0242]
- 
+  
 sc.to_csv('data/temp2.csv', index=False)
